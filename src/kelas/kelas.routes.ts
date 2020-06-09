7eea71892b32973f4b936bf78c758af8ce49e975
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-06-09 21:15:46 +0700

    membuat routing data by id, edit kelas dan hapus kelas

--- src/kelas/kelas.routes.ts
+++ src/kelas/kelas.routes.ts
@@ -2,9 +2,14 @@ import express from "express";
 import {
   ambilData,
   simpanKelas,
+  ambilDataById,
+  editKelas,
+  hapusKelas,
 } from "./kelas.controller";
 
 const router = express.Router();
 
 router.route("/").get(ambilData).post(simpanKelas);
+router.route("/:id").get(ambilDataById).put(editKelas).delete(hapusKelas);
+
 export default router;

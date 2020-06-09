f97945dea81651d50f177a7cfe39b32e7c043602
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-06-09 21:12:55 +0700

    set up server app

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+// Routes
+import KelasRoutes from "./kelas/kelas.routes";
+
+class App {
+  app: Application;
+
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  private routes() {
+    this.app.use("/kelas", KelasRoutes);
+  }
+
+  private middlewares() {
+    this.app.use(morgan("dev"));
+    this.app.use(express.json());
+    // this.app.use(cors());
+  }
+
+  async listen(): Promise<void> {
+    await this.app.listen(3000);
+    console.log("Server is running on port 3000");
+  }
+}
+
+const app = new App();
+app.listen();

678c0c02440d2b95573610d800adcac29c2e38c2
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-06-09 21:58:09 +0700

    membuat fungsi ambil data by id/nis di siswa controller

--- src/siswa/siswa.controller.ts
+++ src/siswa/siswa.controller.ts
@@ -8,4 +8,14 @@ export class SiswaController {
     const siswa = await conn.query("SELECT * FROM siswa");
     return res.status(200).json(siswa[0]);
   }
+
+  async ambilById(req: Request, res: Response) {
+    const nis = req.params.nis;
+    const conn = await connect();
+    const siswa = await conn.query(
+      "SELECT * FROM siswa JOIN kelas ON kelas.id = siswa.kelas_id WHERE nis = ? ",
+      [nis]
+    );
+    return res.status(200).json(siswa[0]);
+  }
 }
